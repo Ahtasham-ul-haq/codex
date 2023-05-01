b7dba8055cc2b87c061a37fead5efa14f9b8dbc9
Ahtasham-ul-haq <ahtasham1802@gmail.com>
2023-05-01 23:41:01 +0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import React, {useEffect, useState} from 'react'
+import { Layout } from '../components'
 import '@/styles/globals.scss'
 
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import Image from "next/image";
 import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
+
+// const posts = [
+//   { title: "React Testing", excerpt: "Learn React Testing" },
+//   { title: "React with Tailwind", excerpt: "Learn React with tailwind" }
+// ];
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10">
+    <div className="container mx-auto px-10 m-8">
       <Head>
         <title>Codex</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-spa4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
