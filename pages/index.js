59ef89b314dd478f26ec3de13e20e936aaa547cd
Zeshanulhaq <zeshanulhaq1655@gmail.com>
2023-05-13 16:13:32 +0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "@/components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with tailwind" },
-];
+// const posts = [
+//   { title: "React Testing", excerpt: "Learn React Testing" },
+//   { title: "React with Tailwind", excerpt: "Learn React with tailwind" },
+// ];
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 m-8">
-      <Head>
+      <Head> 
         <title>Codex</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map(post => (
-            <PostCard post={post} key={post.title} />
-          ))}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} /> )}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -29,3 +28,10 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  }
+};
