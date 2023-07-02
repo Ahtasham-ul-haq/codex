27c7943b3a2d432e2511c88a367af0bdea98ad60
Ahtahsam <ahtasham1802.com>
2023-07-02 09:54:31 +0500

    final-changes

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-import { FeaturedPostCard } from '../components';
 import { getFeaturedPosts } from '../services';
+import { FeaturedPostCard } from '../components';
 
 const responsive = {
     superLargeDesktop: {
