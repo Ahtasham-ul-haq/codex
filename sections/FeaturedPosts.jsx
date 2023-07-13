700f4b02832b744af37fabcc7d115c4d186d12a0
Ahtahsam <ahtasham1802.com>
2023-07-13 13:20:58 +0500

    final-changes

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -53,11 +53,11 @@ const FeaturedPosts = () => {
 
     return (
         <div className="mb-8">
-            <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
+            {/* <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
                 {dataLoaded && featuredPosts.map((post, index) => (
                     <FeaturedPostCard key={index} post={post} />
                 ))}
-            </Carousel>
+            </Carousel> */}
         </div>
     );
 };
